d3ab71d45980ec782b2ddf6122b9735810aee2cc
Greg K. <grrstine@gmail.com>
2026-04-03 15:08:51 -0700

    Update ampcmd formula with SHA256

--- Formula/ampcmd.rb
+++ Formula/ampcmd.rb
@@ -2,46 +2,42 @@ class Ampcmd < Formula
   desc "Chain multiple shell history commands using fuzzy selection"
   homepage "https://github.com/soundslikeinfo/ampcmd"
   url "https://github.com/soundslikeinfo/ampcmd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "ec136f6546e0230393aef4becdc29e9d52d2a6d7f7324087f16653fe724f06d8"
   license "MIT"
   head "https://github.com/soundslikeinfo/ampcmd.git", branch: "main"
 
   depends_on "fzf"
 
   def install
-    zsh_dir = prefix/"zsh"
-    bash_dir = prefix/"bash"
+    prefix.install "src/ampcmd.zsh"
+    prefix.install "src/ampcmd.plugin.zsh"
+    prefix.install "src/ampcmd.bash"
+    prefix.install "src/ampcmd-preview.sh"
     
-    zsh_dir.install "src/ampcmd.zsh"
-    zsh_dir.install "src/ampcmd.plugin.zsh"
-    zsh_dir.install "src/ampcmd-preview.sh"
-    
-    bash_dir.install "src/ampcmd.bash"
-    bash_dir.install "src/ampcmd-preview.sh"
-    
-    fish_function = share/"fish/vendor_functions.d/ampcmd.fish"
-    fish_function.install "src/ampcmd.fish"
-    (share/"fish/vendor_functions.d/ampcmd-preview.sh").install "src/ampcmd-preview.sh"
+    (share/"fish/vendor_functions.d").install "src/ampcmd.fish"
+    (share/"fish/vendor_functions.d").install "src/ampcmd-preview.sh"
   end
 
   def caveats
     <<~EOS
-      Add the following to your shell configuration:
+      Add to your shell config:
       
-      For zsh (~/.zshrc):
-        source #{opt_prefix}/zsh/ampcmd.plugin.zsh
+      Zsh (~/.zshrc):
+        source #{opt_prefix}/ampcmd.plugin.zsh
       
-      For bash (~/.bashrc):
-        source #{opt_prefix}/bash/ampcmd.bash
+      Bash (~/.bashrc):
+        source #{opt_prefix}/ampcmd.bash
       
-      For fish (~/.config/fish/config.fish):
+      Fish (~/.config/fish/config.fish):
         bind \\ch 'ampcmd'
       
-      Then reload: exec $SHELL
+      Then: exec $SHELL
     EOS
   end
 
   test do
-    system "#{prefix}/bash/ampcmd.bash", "1"
+    assert_path_exists prefix/"ampcmd.plugin.zsh"
+    assert_path_exists prefix/"ampcmd.bash"
+    assert_path_exists share/"fish/vendor_functions.d/ampcmd.fish"
   end
 end
